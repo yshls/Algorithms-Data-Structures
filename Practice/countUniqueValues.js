cd70913dc4a422962362e808bed824b06ef6540e
yangshsl <shluxnsal01@gmail.com>
2024-08-09 16:04:45 +0900

    고유값 세기 솔류션

--- Practice/countUniqueValues.js
+++ Practice/countUniqueValues.js
@@ -1,12 +1,13 @@
 function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
   let i = 0;
   for (var j = 1; j < arr.length; j++) {
     if (arr[i] !== arr[j]) {
       i++;
       arr[i] = arr[j];
     }
-    console.log(i, j);
   }
+  return i + 1;
 }
 
 countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);
